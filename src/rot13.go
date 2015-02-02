ffb9439dc0c2de69202b62b0a4081e754019c401
Benoit <benoit@hopsandfork.com>
2015-02-02 17:44:28 +0100

    Correct indent

--- src/rot13.go
+++ src/rot13.go
@@ -2,24 +2,23 @@ package rot13
 
 import "strings"
 
+func Rot13(toCrypt string) (crypted string) {
+	alphab := "abcdefghijklmnopqrstuvwxyz"
+	alphabMaj := strings.ToUpper(alphab)
+	alphabet := strings.Split(alphab, "")
+	alphabetMaj := strings.Split(alphabMaj, "")
 
-func Rot13 (toCrypt string) (crypted string) {
-  alphab := "abcdefghijklmnopqrstuvwxyz"
-  alphabMaj := strings.ToUpper(alphab)
-  alphabet := strings.Split(alphab, "")
-  alphabetMaj := strings.Split(alphabMaj, "")
+	lettersToCrypt := strings.Split(toCrypt, "")
 
-  lettersToCrypt := strings.Split(toCrypt, "")
-
-  for _, letterToCrypt := range lettersToCrypt {
-    if strings.Index(alphabMaj, letterToCrypt) >= 0 {
-      crypted += alphabetMaj[(strings.Index(alphabMaj, letterToCrypt) + 13) % 26]
-    } else if strings.Index(alphab, letterToCrypt) >= 0 {
-      crypted += alphabet[(strings.Index(alphab, letterToCrypt) + 13) % 26]
-    } else {
-      crypted += letterToCrypt
-    }
-  }
-  return crypted;
+	for _, letterToCrypt := range lettersToCrypt {
+		if strings.Index(alphabMaj, letterToCrypt) >= 0 {
+			crypted += alphabetMaj[(strings.Index(alphabMaj, letterToCrypt)+13)%26]
+		} else if strings.Index(alphab, letterToCrypt) >= 0 {
+			crypted += alphabet[(strings.Index(alphab, letterToCrypt)+13)%26]
+		} else {
+			crypted += letterToCrypt
+		}
+	}
+	return crypted
 
 }
--- src/rot13_test.go
+++ src/rot13_test.go
@@ -3,24 +3,23 @@ package rot13
 import "testing"
 
 func TestRot13(t *testing.T) {
-  tests := []struct {
-    input string
-    expected string
-  } {
-    {input: "a", expected: "n"},
-    {input: "b", expected: "o"},
-    {input: "o", expected: "b"},
-    {input: "A", expected: "N"},
-    {input: "mot", expected: "zbg"},
-    {input: "deux mots", expected: "qrhk zbgf"},
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "a", expected: "n"},
+		{input: "b", expected: "o"},
+		{input: "o", expected: "b"},
+		{input: "A", expected: "N"},
+		{input: "mot", expected: "zbg"},
+		{input: "deux mots", expected: "qrhk zbgf"},
+	}
 
-  }
-
-  for _, testCase := range tests {
-    actual := Rot13(testCase.input)
-    expected := testCase.expected
-    if actual != expected {
-      t.Errorf("%s != %s", actual, expected)
-    }
-  }
+	for _, testCase := range tests {
+		actual := Rot13(testCase.input)
+		expected := testCase.expected
+		if actual != expected {
+			t.Errorf("%s != %s", actual, expected)
+		}
+	}
 }
