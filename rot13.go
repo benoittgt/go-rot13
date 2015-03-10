554e1109e4f603523b35ceb6987a19ff448901a0
Benoit <benoit@hopsandfork.com>
2015-03-10 11:13:47 +0100

    Other frEnglish correction

--- rot13.go
+++ rot13.go
@@ -4,15 +4,15 @@ import "strings"
 
 func Rot13(toEncrypt string) (crypted string) {
 	alphab := "abcdefghijklmnopqrstuvwxyz"
-	alphabMaj := strings.ToUpper(alphab)
+	alphabCap := strings.ToUpper(alphab)
 	alphabet := strings.Split(alphab, "")
-	alphabetMaj := strings.Split(alphabMaj, "")
+	alphabetCap := strings.Split(alphabCap, "")
 
 	lettersToCrypt := strings.Split(toEncrypt, "")
 
 	for _, letterToCrypt := range lettersToCrypt {
-		if strings.Index(alphabMaj, letterToCrypt) >= 0 {
-			crypted += alphabetMaj[(strings.Index(alphabMaj, letterToCrypt)+13)%26]
+		if strings.Index(alphabCap, letterToCrypt) >= 0 {
+			crypted += alphabetCap[(strings.Index(alphabCap, letterToCrypt)+13)%26]
 		} else if strings.Index(alphab, letterToCrypt) >= 0 {
 			crypted += alphabet[(strings.Index(alphab, letterToCrypt)+13)%26]
 		} else {
